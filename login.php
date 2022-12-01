62b60148ffb4ecb959a8670ab8a64a71fce6fa05
Fabian Sponholz <s4faspon@uni-trier.de>
2022-12-01 17:50:51 +0100

    Login-Seite realisiert.

--- login.php
+++ login.php
@@ -11,9 +11,28 @@
         <div class="bg-light mt-3 mb-3 p-5">
             <h1 class="text-center">Aufgabenplaner: Login</h1>
         </div>
-        <div class="row">
-
-
+        <div class="container w-50">
+            <form>
+                <div class="form-group my-1">
+                    <label for="loginEmail">Email-Adresse:</label>
+                    <input id="loginEmail" type="text" class="form-control" placeholder="Email-Adresse eingeben">
+                </div>
+                <div class="form-group my-1">
+                    <label for="loginPwd">Passwort:</label>
+                    <input id="loginPwd" type="password" class="form-control" placeholder="Passwort">
+                </div>
+                <div class="form-group form-check my-1">
+                    <input id="agbCheck" type="checkbox" class="form-check-input">
+                    <label for="agbCheck" class="form-check-label">AGBs und Datenschutzbedingungen akzeptieren</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Einloggen</button>
+                <div class="form-group">
+                    Noch nicht registriert? <a class="link-primary text-decoration-none" href="">Registrierung</a>
+                </div>
+                <div class="form-group mt-2">
+                    Da der Login-Vorgang noch nicht realisiert wurde: <a class="link-primary text-decoration-none" href="./todos.php">Überspringen</a>
+                </div>
+            </form>
         </div>
     </div>
 </body>
