c9bc6dd960551171b51819bc1f34aacf08723cd0
Fabian Sponholz <s4faspon@uni-trier.de>
2022-12-01 23:25:53 +0100

    Dynamische Anpassung von login.php an die Fenstergröße

--- login.php
+++ login.php
@@ -11,28 +11,31 @@
         <div class="bg-light mt-3 mb-3 p-5">
             <h1 class="text-center">Aufgabenplaner: Login</h1>
         </div>
-        <div class="container w-50">
-            <form>
-                <div class="form-group my-1">
-                    <label for="loginEmail">Email-Adresse:</label>
-                    <input id="loginEmail" type="text" class="form-control" placeholder="Email-Adresse eingeben">
-                </div>
-                <div class="form-group my-1">
-                    <label for="loginPwd">Passwort:</label>
-                    <input id="loginPwd" type="password" class="form-control" placeholder="Passwort">
-                </div>
-                <div class="form-group form-check my-1">
-                    <input id="agbCheck" type="checkbox" class="form-check-input">
-                    <label for="agbCheck" class="form-check-label">AGBs und Datenschutzbedingungen akzeptieren</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Einloggen</button>
-                <div class="form-group">
-                    Noch nicht registriert? <a class="link-primary text-decoration-none" href="">Registrierung</a>
-                </div>
-                <div class="form-group mt-2">
-                    Da der Login-Vorgang noch nicht realisiert wurde: <a class="link-primary text-decoration-none" href="./todos.php">Überspringen</a>
-                </div>
-            </form>
+        <div class="row">
+            <div class="col-xxl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1"></div>
+            <div class="col-xxl-4 col-lg-6 col-md-8 col-sm-10 col-xs-10">
+                <form>
+                    <div class="form-group my-1">
+                        <label for="loginEmail">Email-Adresse:</label>
+                        <input id="loginEmail" type="text" class="form-control" placeholder="Email-Adresse eingeben">
+                    </div>
+                    <div class="form-group my-1">
+                        <label for="loginPwd">Passwort:</label>
+                        <input id="loginPwd" type="password" class="form-control" placeholder="Passwort">
+                    </div>
+                    <div class="form-group form-check my-1">
+                        <input id="agbCheck" type="checkbox" class="form-check-input">
+                        <label for="agbCheck" class="form-check-label">AGBs und Datenschutzbedingungen akzeptieren</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Einloggen</button>
+                    <div class="form-group">
+                        Noch nicht registriert? <a class="link-primary text-decoration-none" href="">Registrierung</a>
+                    </div>
+                    <div class="form-group mt-2">
+                        Da der Login-Vorgang noch nicht realisiert wurde: <a class="link-primary text-decoration-none" href="./todos.php">Überspringen</a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
