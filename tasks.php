33f587af8c3a6a79ae781fee6b90a04afaf0fd67
Fabian Sponholz <s4faspon@uni-trier.de>
2022-12-06 16:37:50 +0100

    Aufgaben-Seite realisiert.

--- tasks.php
+++ tasks.php
@@ -1,10 +1,46 @@
+<?php
+$aufgaben = array(
+    array(
+        "name" => "HTML Datei erstellen",
+        "beschreibung" => "HTML Datei erstellen",
+        "reiter" => "ToDo",
+        "zustaendig" => "Max Mustermann"
+    ),
+    array(
+        "name" => "CSS Datei erstellen",
+        "beschreibung" => "CSS Datei erstellen",
+        "reiter" => "ToDo",
+        "zustaendig" => "Max Mustermann"
+    ),
+    array(
+        "name" => "PC eingeschaltet",
+        "beschreibung" => "PC einschalten",
+        "reiter" => "Erledigt",
+        "zustaendig" => "Max Mustermann"
+    ),
+    array(
+        "name" => "Kaffee trinken",
+        "beschreibung" => "Kaffee trinken",
+        "reiter" => "Erledigt",
+        "zustaendig" => "Petra Müller"
+    ),
+    array(
+        "name" => "Für die Uni lernen",
+        "beschreibung" => "Für die Uni lernen",
+        "reiter" => "Verschoben",
+        "zustaendig" => "Max Mustermann"
+    )
+)
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Aufgaben</title>
-    <link href="https://unpkg.com/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-          rel="stylesheet" />
+    <title>Reiter</title>
+    <link href="https://unpkg.com/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <script src="https://kit.fontawesome.com/092bee193d.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container-fluid">
@@ -14,7 +50,65 @@
     ?>
     <div class="row">
         <?php require("./common/sidebar.php"); ?>
-
+        <div class="col-8">
+            <table class="table table-hover mb-5">
+                <thead>
+                <tr class="table-light">
+                    <th scope="col">Aufgabenbezeichnung:</th>
+                    <th scope="col">Beschreibung der Aufgabe:</th>
+                    <th scope="col">Reiter:</th>
+                    <th scope="col">Zuständig:</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if (isset($aufgaben)): foreach ($aufgaben as $item): ?>
+                    <tr>
+                        <td><?php echo(isset($item['name']) ? $item['name'] : ""); ?></td>
+                        <td><?php echo(isset($item['beschreibung']) ? $item['beschreibung'] : ""); ?></td>
+                        <td><?php echo(isset($item['reiter']) ? $item['reiter'] : ""); ?></td>
+                        <td><?php echo(isset($item['zustaendig']) ? $item['zustaendig'] : ""); ?></td>
+                        <td class="text-end"><i class="fa-regular fa-trash-can text-primary m-3"></i> <i class="fa-regular fa-pen-to-square text-primary m-3"></i></td>
+                    </tr>
+                <?php endforeach; endif;?>
+                </tbody>
+            </table>
+            <form>
+                <h2>Bearbeiten/Erstellen</h2>
+                <div class="form-group mb-2">
+                    <label for="aufgabeBezeichnungInput" class="mb-1">Aufgabenbezeichnung:</label>
+                    <input type="text" class="form-control" id="aufgabeBezeichnungInput" placeholder="Aufgabe">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="aufgabeBeschreibungText" class="mb-1">Beschreibung der Aufgabe:</label>
+                    <textarea id="aufgabeBeschreibungText" rows="4" class="form-control" placeholder="Beschreibung"></textarea>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="erstellungsDatumInput" class="mb-1">Erstellungsdatum:</label>
+                    <input type="date" class="form-control" id="erstellungsDatumInput">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="reiterBezeichnungInput" class="mb-1">fällig bis:</label>
+                    <input type="date" class="form-control" id="reiterBezeichnungInput">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="reiterMenu" class="mb-1">Zugehöriger Reiter:</label>
+                    <select id="reiterMenu" class="form-select">
+                        <option label="ToDo"></option>
+                    </select>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="zustaendigMenu" class="mb-1">Zuständig:</label>
+                    <select id="zustaendigMenu" class="form-select">
+                        <option label="Max Mustermann"></option>
+                    </select>
+                </div>
+                <button class="btn btn-primary">Speichern</button>
+                <button class="btn btn-info text-white">Reset</button>
+            </form>
+        </div>
     </div>
 </div>
+<br>
+<br>
 </body>
