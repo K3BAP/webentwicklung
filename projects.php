0817ef7fa2a5dd8230191f85eccf06bd33cf5fa9
Fabian Sponholz <s4faspon@uni-trier.de>
2022-12-06 14:04:43 +0100

    Projekte-Seite realisiert.

--- projects.php
+++ projects.php
@@ -15,6 +15,30 @@
     <div class="row">
         <?php require("./common/sidebar.php"); ?>
 
+        <div class="col-8">
+            <form class="mb-3">
+                <h2>Projekt auswählen:</h2>
+                <select class="form-select">
+                    <option label="- bitte auswählen -"></option>
+                </select>
+                <button class="btn btn-primary mt-3">Auswählen</button>
+                <button class="btn btn-primary mt-3">Bearbeiten</button>
+                <button class="btn btn-danger mt-3">Löschen</button>
+            </form>
+            <form>
+                <h2>Projekt bearbeiten/erstellen:</h2>
+                <div class="form-group mb-3">
+                    <label class="form-label" for="projektInput">Projektname:</label>
+                    <input class="form-control" id="projektInput" placeholder="Projekt">
+                </div>
+                <div class="form-group mb-3">
+                    <label class="form-label" for="beschreibungTextArea">Projektbeschreibung:</label>
+                    <textarea class="form-control" id="beschreibungTextArea" placeholder="Beschreibung" rows="4"></textarea>
+                </div>
+                <button class="btn btn-primary">Speichern</button>
+                <button class="btn btn-info text-white">Reset</button>
+            </form>
+        </div>
     </div>
 </div>
 </body>
