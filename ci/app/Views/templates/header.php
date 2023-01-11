cecd683db91ff834c02dc46a7a5d1127e1815ac3
Fabian Sponholz <s4faspon@uni-trier.de>
2023-01-11 19:16:35 +0100

    Bootstrap update auf 5.2.3, bootstrap JS eingebunden

--- ci/app/Views/templates/header.php
+++ ci/app/Views/templates/header.php
@@ -6,7 +6,8 @@
 
     <title><?php echo $title; ?></title>
 
-    <link href="https://unpkg.com/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 
     <script src="https://kit.fontawesome.com/092bee193d.js" crossorigin="anonymous"></script>
 </head>
