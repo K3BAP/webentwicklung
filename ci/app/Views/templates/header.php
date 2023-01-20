a0ee113adf2adc0a95b7b8ea0ae52d59efc46375
Fabian Sponholz <s4faspon@uni-trier.de>
2023-01-20 17:50:57 +0100

    Bootstrap lokal eingebunden anstatt über externen reflink

--- ci/app/Views/templates/header.php
+++ ci/app/Views/templates/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?php echo $title; ?></title>
+    <title><?= $title ?></title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="<?= base_url('/CSS/bootstrap.min.css') ?>" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 
     <script src="https://kit.fontawesome.com/092bee193d.js" crossorigin="anonymous"></script>
@@ -14,6 +14,6 @@
 <body>
     <div class="container-fluid">
         <div class="bg-light mt-3 mb-3 p-5">
-            <h1 class="text-center"><?php echo $heading; ?></h1>
+            <h1 class="text-center"><?= $heading; ?></h1>
         </div>
         <div class="row">
