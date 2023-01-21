7e34b903f7dde7f8bb55e8936e39a98c613f4296
Fabian Sponholz <s4faspon@uni-trier.de>
2023-01-21 10:52:31 +0100

    Verfügbare Projekte werden im Dropdown angezeigt. Modaldialog zum Hinzufügen von Projekten, noch ohne Funktion. Formular von Seite entfernt, ist jetzt im Modaldialog

--- ci/app/Controllers/Projects.php
+++ ci/app/Controllers/Projects.php
@@ -2,20 +2,28 @@
 
 namespace App\Controllers;
 
-use App\Models\AufgabeModel;
-use App\Models\MitgliedModel;
-use App\Models\ReiterModel;
+use App\Models\ProjektModel;
 
 class Projects extends BaseController
 {
+    private ProjektModel $projektModel;
+
+    public function __construct() {
+
+        $this->projektModel = new ProjektModel();
+
+        helper(["form", "url"]);
+    }
     public function index()
     {
         $headData['title'] = 'Projekte';
         $headData['heading'] = 'Aufgabenplaner: Projekte';
 
+        $data['projects'] = $this->projektModel->getProjekte();
+
         echo view('templates/header', $headData);
         echo view('templates/sidebar');
-        echo view('projects');
+        echo view('projects', $data);
         echo view('templates/footer');
     }
 }
--- ci/app/Views/projects.php
+++ ci/app/Views/projects.php
@@ -1,25 +1,52 @@
+<!-- MODAL CREATE / EDIT DIALOG -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Bearbeiten/Erstellen</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <?= form_open("projects/save", array('role' => 'form'))?>
+            <div class="modal-body">
 
+                <input type="hidden" id="projektIdInput" name="projektId" readonly>
+                <div class="form-group mb-3">
+                    <label class="form-label" for="projektInput">Projektname:</label>
+                    <input class="form-control" id="projektInput" placeholder="Projekt">
+                </div>
+                <div class="form-group mb-3">
+                    <label class="form-label" for="beschreibungTextArea">Projektbeschreibung:</label>
+                    <textarea class="form-control" id="beschreibungTextArea" placeholder="Beschreibung" rows="4"></textarea>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+                <button id="saveBtn" type="submit" class="btn btn-primary" >Speichern</button>
+            </div>
+            <?= form_close() ?>
+        </div>
+    </div>
+</div>
+
+<!-- PAGE CONTENT -->
 <div class="col-8">
     <form class="mb-3">
         <h2>Projekt auswählen:</h2>
         <select class="form-select">
             <option label="- bitte auswählen -"></option>
+            <?php if(!empty($projects)): foreach($projects as $item): ?>
+            <option value="<?= $item['projektId'] ?>" label="<?= $item['projektName'] ?>"></option>
+            <?php endforeach; endif; ?>
         </select>
         <button class="btn btn-primary mt-3">Auswählen</button>
         <button class="btn btn-primary mt-3">Bearbeiten</button>
+        <a
+                class="btn btn-primary mt-3"
+                role="button"
+                data-bs-toggle="modal"
+                data-bs-target="#editModal"
+        >Neu</a>
         <button class="btn btn-danger mt-3">Löschen</button>
-    </form>
-    <form>
-        <h2>Projekt bearbeiten/erstellen:</h2>
-        <div class="form-group mb-3">
-            <label class="form-label" for="projektInput">Projektname:</label>
-            <input class="form-control" id="projektInput" placeholder="Projekt">
-        </div>
-        <div class="form-group mb-3">
-            <label class="form-label" for="beschreibungTextArea">Projektbeschreibung:</label>
-            <textarea class="form-control" id="beschreibungTextArea" placeholder="Beschreibung" rows="4"></textarea>
-        </div>
-        <button class="btn btn-primary">Speichern</button>
-        <button class="btn btn-info text-white">Reset</button>
     </form>
 </div>
