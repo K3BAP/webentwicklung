826c65e4ee854546f7417291734021e593a47a43
Fabian Sponholz <s4faspon@uni-trier.de>
2023-01-22 21:38:36 +0100

    Konfigänderung, um direkt auf den Public-Ordner umzuleiten (kein /ci/public/ in der URL)

--- ci/app/Config/App.php
+++ ci/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/webentwicklung/ci/public/';
+    public $baseURL = 'http://localhost/webentwicklung/';
 
     /**
      * --------------------------------------------------------------------------
@@ -37,7 +37,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
