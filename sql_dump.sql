9cb9342e8d2e475822454a7d8053f1ecbc6707d4
Fabian Sponholz <s4faspon@uni-trier.de>
2023-01-29 00:38:01 +0100

    Serverkonfiguration angepasst. SQL dump erneuert, da letzter nicht importiert werden konnte

--- sql_dump.sql
+++ sql_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 27. Jan 2023 um 16:13
+-- Erstellungszeit: 27. Jan 2023 um 21:41
 -- Server-Version: 10.4.27-MariaDB
 -- PHP-Version: 8.2.0
 
@@ -46,7 +46,115 @@ INSERT INTO `aufgabe` (`aufgabeId`, `aufgabeName`, `aufgabeBeschreibung`, `aufga
 (2, 'CSS Datei erstellen', 'CSS Datei erstellen', '2022-12-13', NULL, 1, 1),
 (3, 'PC eingeschaltet', 'PC einschalten', '2022-12-13', NULL, 1, 2),
 (4, 'Kaffee trinken', 'Kaffee trinken fördert die Denkfähigkeit.', '2022-12-13', NULL, 1, 2),
-(5, 'Für die Uni lernen', 'Für die Uni lernen ist auch wichtig.', '2022-12-13', NULL, 1, 3);
+(5, 'Für die Uni lernen', 'Für die Uni lernen ist auch wichtig.', '2022-12-13', NULL, 1, 3),
+(7, 'Tolle Aufgabe', 'Hier sind Petra und Max zuständig', '2023-01-27', '2023-02-03', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `aufgabe_mitglied`
+--
+
+CREATE TABLE `aufgabe_mitglied` (
+  `aufgabeId` int(10) UNSIGNED NOT NULL,
+  `mitgliedId` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `aufgabe_mitglied`
+--
+
+INSERT INTO `aufgabe_mitglied` (`aufgabeId`, `mitgliedId`) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 1),
+(5, 2),
+(7, 1),
+(7, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `mitglied`
+--
+
+CREATE TABLE `mitglied` (
+  `mitgliedId` int(10) UNSIGNED NOT NULL,
+  `mitgliedUsername` varchar(50) NOT NULL,
+  `mitgliedEmail` varchar(50) NOT NULL,
+  `mitgliedPassword` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `mitglied`
+--
+
+INSERT INTO `mitglied` (`mitgliedId`, `mitgliedUsername`, `mitgliedEmail`, `mitgliedPassword`) VALUES
+(1, 'Max Mustermann', 'mustermann@muster.de', '$2y$10$..fQtlR6xRvVcHNRV/eN.uZ8DqJ8.BaDGQi7n8BdfR8cUIiODdpv6'),
+(2, 'Petra Müller', 'petra@mueller.de', '$2y$10$..fQtlR6xRvVcHNRV/eN.uZ8DqJ8.BaDGQi7n8BdfR8cUIiODdpv6');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `projekt`
+--
+
+CREATE TABLE `projekt` (
+  `projektId` int(10) UNSIGNED NOT NULL,
+  `projektName` varchar(50) NOT NULL,
+  `projektBeschreibung` text NOT NULL,
+  `projektErstellerMitgliedId` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `projekt`
+--
+
+INSERT INTO `projekt` (`projektId`, `projektName`, `projektBeschreibung`, `projektErstellerMitgliedId`) VALUES
+(1, 'Großes Gesamtprojekt', 'Dieses Projekt befasst sich mit der Lösung der endgültigen Frage nach dem Leben, dem Universum und dem ganzen Rest.', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `projekt_mitglied`
+--
+
+CREATE TABLE `projekt_mitglied` (
+  `projektId` int(10) UNSIGNED NOT NULL,
+  `mitgliedId` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `projekt_mitglied`
+--
+
+INSERT INTO `projekt_mitglied` (`projektId`, `mitgliedId`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `reiter`
+--
+
+CREATE TABLE `reiter` (
+  `reiterId` int(10) UNSIGNED NOT NULL,
+  `reiterProjektId` int(10) UNSIGNED NOT NULL,
+  `reiterName` varchar(100) NOT NULL,
+  `reiterBeschreibung` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `reiter`
+--
+
+INSERT INTO `reiter` (`reiterId`, `reiterProjektId`, `reiterName`, `reiterBeschreibung`) VALUES
+(1, 1, 'ToDo', 'Dinge, die erledigt werden müssen'),
+(2, 1, 'Erledigt', 'Dinge, die erledigt sind'),
+(3, 1, 'Verschoben', 'Dinge, die später erledigt werden');
 
 --
 -- Indizes der exportierten Tabellen
@@ -61,6 +169,41 @@ ALTER TABLE `aufgabe`
   ADD KEY `aufgabeReiterId` (`aufgabeReiterId`);
 
 --
+-- Indizes für die Tabelle `aufgabe_mitglied`
+--
+ALTER TABLE `aufgabe_mitglied`
+  ADD PRIMARY KEY (`aufgabeId`,`mitgliedId`),
+  ADD KEY `aufgabe_mitglied_ibfk_2` (`mitgliedId`);
+
+--
+-- Indizes für die Tabelle `mitglied`
+--
+ALTER TABLE `mitglied`
+  ADD PRIMARY KEY (`mitgliedId`),
+  ADD UNIQUE KEY `mitgliedEmail` (`mitgliedEmail`);
+
+--
+-- Indizes für die Tabelle `projekt`
+--
+ALTER TABLE `projekt`
+  ADD PRIMARY KEY (`projektId`),
+  ADD KEY `erstellerMitgliedId` (`projektErstellerMitgliedId`);
+
+--
+-- Indizes für die Tabelle `projekt_mitglied`
+--
+ALTER TABLE `projekt_mitglied`
+  ADD PRIMARY KEY (`projektId`,`mitgliedId`),
+  ADD KEY `projekt_mitglied_ibfk_1` (`mitgliedId`);
+
+--
+-- Indizes für die Tabelle `reiter`
+--
+ALTER TABLE `reiter`
+  ADD PRIMARY KEY (`reiterId`),
+  ADD KEY `reiterProjektId` (`reiterProjektId`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
@@ -68,7 +211,25 @@ ALTER TABLE `aufgabe`
 -- AUTO_INCREMENT für Tabelle `aufgabe`
 --
 ALTER TABLE `aufgabe`
-  MODIFY `aufgabeId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `aufgabeId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT für Tabelle `mitglied`
+--
+ALTER TABLE `mitglied`
+  MODIFY `mitgliedId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT für Tabelle `projekt`
+--
+ALTER TABLE `projekt`
+  MODIFY `projektId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT für Tabelle `reiter`
+--
+ALTER TABLE `reiter`
+  MODIFY `reiterId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints der exportierten Tabellen
@@ -80,6 +241,32 @@ ALTER TABLE `aufgabe`
 ALTER TABLE `aufgabe`
   ADD CONSTRAINT `aufgabe_ibfk_1` FOREIGN KEY (`aufgabeErstellerMitgliedId`) REFERENCES `mitglied` (`mitgliedId`) ON DELETE SET NULL,
   ADD CONSTRAINT `aufgabe_ibfk_2` FOREIGN KEY (`aufgabeReiterId`) REFERENCES `reiter` (`reiterId`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `aufgabe_mitglied`
+--
+ALTER TABLE `aufgabe_mitglied`
+  ADD CONSTRAINT `aufgabe_mitglied_ibfk_1` FOREIGN KEY (`aufgabeId`) REFERENCES `aufgabe` (`aufgabeId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `aufgabe_mitglied_ibfk_2` FOREIGN KEY (`mitgliedId`) REFERENCES `mitglied` (`mitgliedId`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `projekt`
+--
+ALTER TABLE `projekt`
+  ADD CONSTRAINT `projekt_ibfk_1` FOREIGN KEY (`projektErstellerMitgliedId`) REFERENCES `mitglied` (`mitgliedId`) ON DELETE SET NULL;
+
+--
+-- Constraints der Tabelle `projekt_mitglied`
+--
+ALTER TABLE `projekt_mitglied`
+  ADD CONSTRAINT `projekt_mitglied_ibfk_1` FOREIGN KEY (`mitgliedId`) REFERENCES `mitglied` (`mitgliedId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `projekt_mitglied_ibfk_2` FOREIGN KEY (`projektId`) REFERENCES `projekt` (`projektId`) ON DELETE CASCADE;
+
+--
+-- Constraints der Tabelle `reiter`
+--
+ALTER TABLE `reiter`
+  ADD CONSTRAINT `reiter_ibfk_1` FOREIGN KEY (`reiterProjektId`) REFERENCES `projekt` (`projektId`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
